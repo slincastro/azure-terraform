1cee96f2f065c42a8997d7ed47643b49fee1db4f
Slin Castro <scastro@thoughtworks.com>
2021-02-05 20:11:00 -0500

    Include terraform file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # azure-terraform
+
+> Install Azure CLI :
+
+`brew update && brew install azure-cli`
+
+`az login`
+
+> For macOS :
+
+`brew install terraform`
+
+> Start terraform : 
+
+`terraform init`
+
+`terraform plan -out test.tfplan`
---
+++ app-insights.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  version = "=2.46.1"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "tf-test"
+  location = "West US"
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.example.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.example.app_id
+}
